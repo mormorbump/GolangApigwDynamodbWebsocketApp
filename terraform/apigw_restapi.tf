9b4a1eec0e4856385f66d9a5fe4ab37ac63de209
kazuki matsumoto <mormorbump@gmail.com>
2023-04-21 14:32:53 +0900

    余分な記述を削除

--- terraform/apigw_restapi.tf
+++ terraform/apigw_restapi.tf
@@ -25,11 +25,6 @@ resource "aws_api_gateway_deployment" "default" {
       aws_api_gateway_method.send_message_post,
       aws_api_gateway_integration.send_message_post,
       aws_api_gateway_method_response.send_message_response_200,
-
-      aws_api_gateway_resource.send_gift,
-      aws_api_gateway_method.send_gift_post,
-      aws_api_gateway_integration.send_gift_post,
-      aws_api_gateway_method_response.send_gift_response_200,
     ]))
   }
 
@@ -68,39 +63,5 @@ resource "aws_api_gateway_method_response" "send_message_response_200" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   resource_id = aws_api_gateway_resource.send_message.id
   http_method = aws_api_gateway_method.send_message_post.http_method
-  status_code = "200"
-}
-
-
-################################################################################
-# Path  : /send_gift                                                           #
-# Method: POST                                                                 #
-################################################################################
-resource "aws_api_gateway_resource" "send_gift" {
-  path_part   = "send_gift"
-  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-}
-
-resource "aws_api_gateway_method" "send_gift_post" {
-  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-  resource_id   = aws_api_gateway_resource.send_gift.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "send_gift_post" {
-  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
-  resource_id             = aws_api_gateway_resource.send_gift.id
-  http_method             = aws_api_gateway_method.send_gift_post.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.send_gift.invoke_arn
-}
-
-resource "aws_api_gateway_method_response" "send_gift_response_200" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  resource_id = aws_api_gateway_resource.send_gift.id
-  http_method = aws_api_gateway_method.send_gift_post.http_method
   status_code = "200"
 }
